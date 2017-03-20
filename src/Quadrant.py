2f85e77e1058eac9739e087a1982574b88ddae91
Oakular <cal.warrilow@gmail.com>
2017-03-20 14:44:00 +0000

    begun implementation of Quadrant class

---
+++ src/Quadrant.py
@@ -0,0 +1,39 @@
+# --- A Class to represent a Quadrant in the GUI.
+# --- Each Quadrant extends the GTKTextView Class
+# --- Author: Callum Warrilow
+
+from gi.repository import Gtk
+
+class Quadrant(Gtk.TextView):
+
+    FILE_PATH;
+
+    # --- CONSTRUCTOR
+    def __init__(self):
+        Gtk.TextView.__init__(self)
+        self.set_wrap_mode(Gtk.WrapMode.WORD)
+    # end CONSTRUCTOR
+
+    # --- CONSTRUCTOR (sets file path within constructor)
+    def __init__(self, filePath, quadNum):
+        Gtk.TextView.__init__(self)
+        self.set_wrap_mode(Gtk.WrapMode.WORD)
+        setFilePath(filePath, quadNum)
+    # end CONSTRUCTOR
+
+    # --- method to get the path for the file containing text for this quadrant
+    def getFilePath():
+    # end getFilePath()
+
+    # --- method to set the path for the file containing text for this quadrant
+    def setFilePath(filePath, quadNum):
+    # end setFilePath()
+
+    # --- method to read the contents of the file for this quadrant
+    def readFromFile():
+    # end readFromFile()
+
+    # --- method to write the contents of the file for this quadrant from the
+    # text buffer
+    def writeToFile():
+    # end writeToFile()
