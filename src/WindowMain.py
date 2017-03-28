59d0a6140151691136fe990179b8866b5a4fee6f
oakular <callum@oakular.com>
2017-03-28 11:09:00 +0100

    added quadrant creation method in WindowMain

--- src/WindowMain.py
+++ src/WindowMain.py
@@ -12,13 +12,32 @@ class QuadWindow(Gtk.Window):
     def __init__(self):
         Gtk.Window.__init__(self)
         quadGrid = Gtk.Grid()
+        self.add(quadGrid)
+
+        self.createGUI()
 
     # --- method for making other method calls to setup the whole GUI
-    def createGUI():
+    def createGUI(self):
+        self.createQuad()
         return
     # end createGUI()
 
     # --- method to specifically create an instance of Quadrant on the Window
     def createQuad(self):
-        return
+        # create quadrants
+        quad0 = Quadrant(quadPath, 0)
+        quad0.readFromFile()
+        grid.add(quad0)
+
+        quad1 = Quadrant(quadPath, 1)
+        quad1.readFromFile()
+        grid.attach_next_to(quad1, quad0, Gtk.PositionType.RIGHT, 1, 1)
+
+        quad2 = Quadrant(quadPath, 2)
+        quad2.readFromFile()
+        grid.attach_next_to(quad2, quad0, Gtk.PositionType.BOTTOM, 1, 1)
+
+        quad3 = Quadrant(quadPath, 3)
+        quad3.readFromFile()
+        grid.attach_next_to(quad3, quad1, Gtk.PositionType.BOTTOM, 1, 1)
     # end createQuad()
